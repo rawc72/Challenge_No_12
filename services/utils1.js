5fc2b003a7b7ffafa498ca872722e81a66c7f0ad
Robert Anthony Williams <rawc@me.com>
2021-12-08 16:03:23 -0600

    Second commit. Additional .js coding plus the .env file

--- services/utils1.js
+++ services/utils1.js
@@ -62,3 +62,81 @@ addDepartment = async(pool) => {
         console.log("Insert new department error: " + JSON.stringify(error));
     }
 };
+
+updateEmployeeRole = async(pool) => {
+    // Query database to get all employees
+    const allEmployees =
+        "select id, first_name, last_name, role_id from employee";
+    const result = await pool.query(allEmployees);
+
+    // No employee found
+    if (result[0].length === 0) {
+        console.log("!!!!! No employee exists !!!!!");
+        return;
+    }
+
+    // Get all employees
+    const employees = result[0].map((employee) => ({
+        id: employee.id,
+        name: employee.first_name + " " + employee.last_name,
+        roleId: employee.role_id,
+    }));
+
+    const employeeChoices = employees.map((employee) => employee.name);
+
+    let answer = await inquirer.prompt([{
+        name: "employeeName",
+        type: "list",
+        message: "Who do you want to change role?",
+        choices: [...employeeChoices],
+    }, ]);
+
+    const employeeId = employees.filter(
+        (employee) => employee.name === answer.employeeName
+    )[0].id;
+
+    const oldRoleId = employees.filter(
+        (employee) => employee.name === answer.employeeName
+    )[0].roleId;
+
+    const getOtherRoles = "select id, title from role where id != ?";
+    const parms = [oldRoleId];
+
+    const roleResult = await pool.query(getOtherRoles, parms);
+
+    if (roleResult[0].length === 0) {
+        console.log("!!!!! No other role to choose !!!!!");
+        return;
+    }
+
+    const roleChoices = roleResult[0].map((role) => role.title);
+
+    let answer2 = await inquirer.prompt([{
+        name: "newRole",
+        type: "list",
+        message: "What's the new role for " + answer.employeeName + "?",
+        choices: [...roleChoices],
+    }, ]);
+
+    const newRoleId = roleResult[0].filter(
+        (role) => role.title === answer2.newRole
+    )[0].id;
+
+    const updateRole = "update employee set role_id = ? where id = ?";
+
+    const uParms = [newRoleId, employeeId];
+
+    try {
+        await pool.query(updateRole, uParms);
+        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        console.log(
+            "!!  Update employee " +
+            answer.employeeName +
+            " with new role " +
+            answer2.newRole
+        );
+        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    } catch (error) {
+        console.log("Update employee role error: " + JSON.stringify(error));
+    }
+};
